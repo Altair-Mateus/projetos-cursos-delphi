dd5bfb7253cc49f9036196eb9a52a4fa6b996cfa
Altair-Mateus <taicoalencastro@gmail.com>
2023-04-19 22:53:30 -0300

    Criado tela de cadastro padrão

--- Sistema_Financeiro/src/view/SistemaFinanceiro.View.CadastroPadrao.pas
+++ Sistema_Financeiro/src/view/SistemaFinanceiro.View.CadastroPadrao.pas
@@ -4,10 +4,28 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, System.ImageList, Vcl.ImgList,
+  Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.Grids, Vcl.DBGrids, Vcl.WinXPanels;
 
 type
   TForm1 = class(TForm)
+    CardPanelPrincipal: TCardPanel;
+    CardCadastro: TCard;
+    CardPesquisa: TCard;
+    pnlPesquisa: TPanel;
+    pnlBotoes: TPanel;
+    pnlGrid: TPanel;
+    DBGrid1: TDBGrid;
+    lblPesquisar: TLabel;
+    edtPesquisar: TEdit;
+    btnPesquisae: TButton;
+    Image1: TImage;
+    ImageList1: TImageList;
+    btnIncluir: TButton;
+    btnAlterar: TButton;
+    btnExcluir: TButton;
+    btnImprimir: TButton;
+    btnSair: TButton;
   private
     { Private declarations }
   public
